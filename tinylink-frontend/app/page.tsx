1661d9a904d676875421dde8a7c2b752eabd32a8
vishakha_kasture <vishakhakasture302002@gmail.com>
2025-11-21 17:55:48 +0530

    url

--- tinylink-frontend/app/page.tsx
+++ tinylink-frontend/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const [shortUrl, setShortUrl] = useState("");
 
   async function shortenURL() {
-    const res = await fetch("http://localhost:5000/shorten", {
+    const res = await fetch("https://tinylink-backend.onrender.com/shorten", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ original_url: url }),
